bc2b1ac4674bc0a75b2a7cac030ea7d96c93a6f2
AkrmQ <akrm.codes@gmail.com>
2026-03-09 16:38:27 +0300

    feat: add palyRound function

--- js/rock-paper-scissors/script.js
+++ js/rock-paper-scissors/script.js
@@ -12,6 +12,23 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-    let humanChoice = prompt("Type Your Choice", "rock")
+    let humanChoice = prompt("Type Your Choice", "scissors")
     return humanChoice.toLowerCase();
+}
+
+function playRound(human , computer) {
+    const humanBeatsRock = human === 'paper' && computer === 'rock';
+    const humanBeatsPaper = human === 'scissors' && computer === 'paper';
+    const humanBeatsScissors = human === 'rock' && computer === 'scissors';
+    const drow = human === computer;
+    if (drow)
+        return "It'sDraw"
+    else if (humanBeatsRock)
+        return "human Wins" 
+    else if (humanBeatsPaper)
+        return "human Wins"
+    else if (humanBeatsScissors)
+        return "human Wins"
+    else
+        return "copmuter Wins"
 }
