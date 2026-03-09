9a23a7cb4bbc6d25bdc965d19c45bbdbad5a4d2c
AkrmQ <akrm.codes@gmail.com>
2026-03-09 16:10:31 +0300

    feat: add computer choice function

--- js/rock-paper-scissors/script.js
+++ js/rock-paper-scissors/script.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+function getComputerChoice(){
+    let computerChoice = Math.floor(Math.random() * 10);
+    if (computerChoice <= 3)
+        return "rock"
+    else if(computerChoice >3 && computerChoice <= 6)
+        return "paper"
+    else
+        return "scissors"
+}
