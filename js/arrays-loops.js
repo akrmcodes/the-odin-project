22bc63433058b5b18a36368a931155118e19ec13
AkrmQ <akrm.codes@gmail.com>
2026-03-10 23:17:33 +0300

    ref: refactor calculateTotalDebts function

--- js/arrays-loops.js
+++ js/arrays-loops.js
@@ -1,21 +1,25 @@
 let dailyDebts = [4000, 0, 2500, 0, 1500, 60000, 3000];
 
-let allDebts =0;
-
-const skipedDebts = 0;
-const suspiciousNumber = 50000;
-
-for (i=0 ; i<dailyDebts.length ;i++) {
-    if(dailyDebts[i] === skipedDebts) {
-        console.log("تم تخطي حساب مسدد")
-        continue;
+function calculateTotalDebts(debtsArray) {
+    let allDebts = 0;
+    const skippedDebt = 0;
+    const suspiciousNumber = 50000;
+    for (let i = 0; i < debtsArray.length; i++) {
+        
+        if (debtsArray[i] === skippedDebt) {
+            console.log("تم تخطي حساب مسدد");
+            continue;
+        } 
+        else if (debtsArray[i] >= suspiciousNumber) {
+            console.log("تحذير: تم إيقاف الجرد لوجود رقم مشبوه!");
+            break;
+        } 
+        else {
+            allDebts += debtsArray[i];
+        }
     }
-    else if(dailyDebts[i] >= suspiciousNumber) {
-        console.log("تحذير: تم إيقاف الجرد لوجود رقم مشبوه!")
-        break;
-    }
-    else {
-        allDebts +=dailyDebts[i];
-    }
+        return allDebts;
 }
-console.log(allDebts)
+
+let finalResult = calculateTotalDebts(dailyDebts);
+console.log("الإجمالي السليم المعتمد: " + finalResult);
