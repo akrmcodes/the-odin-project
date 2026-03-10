9bf7bb595cb1339bb0a710850a4205448c7744e0
AkrmQ <akrm.codes@gmail.com>
2026-03-10 13:22:38 +0300

    feat: add clean code unit

---
+++ js/clean-code.js
@@ -0,0 +1,13 @@
+const x= function (z){
+   const w = "Hello ";
+return w +  z
+
+ }
+
+x("John");
+// --------------------------------
+function sayHi (name) {
+    const hi ="Hello"
+    return name +hi
+}
+sayHi("akrm")
